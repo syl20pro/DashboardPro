e46ce2abc9277155b2c14bc8d8b03f0b9df74379
Sylvain Gemieux <44305327+syl20pro@users.noreply.github.com>
2021-08-17 15:09:02 +0200

    add[function]js
    -Ajout des fonctions js pour le menu
    - Ajout des fonctions pour le changement de thème

--- main.js
+++ main.js
@@ -11,6 +11,83 @@ const themeLight = 'light'
 
 const body = document.getElementsByTagName('body')[0]
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+  var expires = "expires="+d.toUTCString()
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+}
+
+function getCookie(cname) {
+  var name = cname + "="
+  var ca = document.cookie.split(';')
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ""
+}
+
+// Light & Dark Theme //
+loadTheme()
+
+function loadTheme() {
+	var theme = getCookie(themeCookieName)
+	body.classList.add(theme === "" ? themeLight : theme)
+}
+
+function switchTheme() {
+	if (body.classList.contains(themeLight)) {
+		body.classList.remove(themeLight)
+		body.classList.add(themeDark)
+		setCookie(themeCookieName, themeDark)
+	} else {
+		body.classList.remove(themeDark)
+		body.classList.add(themeLight)
+		setCookie(themeCookieName, themeLight)
+	}
+}
+
+// Sidebar dropdown //
+
+function collapseSidebar() {
+	body.classList.toggle('sidebar-expand')
+}
+
+window.onclick = function(event) {
+	openCloseDropdown(event)
+}
+
+function closeAllDropdown() {
+	var dropdowns = document.getElementsByClassName('dropdown-expand')
+	for (var i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove('dropdown-expand')
+	}
+}
+
+function openCloseDropdown(event) {
+	if (!event.target.matches('.dropdown-toggle')) {
+		// 
+		// Close dropdown when click out of dropdown menu
+		// 
+		closeAllDropdown()
+	} else {
+		var toggle = event.target.dataset.toggle
+		var content = document.getElementById(toggle)
+		if (content.classList.contains('dropdown-expand')) {
+			closeAllDropdown()
+		} else {
+			closeAllDropdown()
+			content.classList.add('dropdown-expand')
+		}
+	}
+}
+
 // Graphique Chartjs //
 var ctx = document.getElementById('myChart')
 ctx.height = 500
@@ -20,15 +97,15 @@ var data = {
     datasets: [{
         fill: false,
         label: 'Valeur portefeuille',
-        borderColor: successColor,
-        data [6520, 6781, 6824, 6936, 7004, 7088, 7154, 7176, 7182, 7202, 7234, 7284],
+        borderColor: primaryColor,
+        data [6712, 6781, 6824, 6936, 7004, 7088, 7154, 7176, 7182, 7202, 7234, 7284],
         borderWidth: 3,
         lineTension: 0,
     }, {
         fill: false,
         label: 'Dividendes perçus',
-        borderColor: primaryColor,
-        data [232, 261, 43, 112, 68, 84, 66, 22, 6, 20, 32, 50],
+        borderColor: successColor,
+        data [78, 69, 43, 112, 68, 84, 66, 22, 6, 20, 32, 50],
         borderWidth: 3,
         lineTension: 0,
     }]
